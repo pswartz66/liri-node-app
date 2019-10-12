b7fe0887177c025de4e3eeb97a2c8e5fb995cae8
Phil Swartz <pswartz.sju@gmail.com>
2019-10-11 22:56:41 -0400

    added the bands in town search api and created a for loop to grab argv args

--- liri.js
+++ liri.js
@@ -10,12 +10,43 @@ var Spotify = require('./node_modules/node-spotify-api');
 
 var spotify = new Spotify(Keys.spotify);
 
+
+// concatenate argv args into a searchable string
+var args = process.argv;
+var searchArr = [];
+var searchStr = '';
+
+for (var i = 3; i < args.length; i++) {
+
+    searchArr.push(process.argv[i]);
+
+}
+
+searchStr = searchArr.join('+');
+
+
+
+
+// third arg in argv arguments
 var input = process.argv[2];
 
 switch (input) {
     case 'concert-this':
         // do something
         console.log('You chose concert-this');
+        console.log('-------------Sapmple Output--------------')
+
+
+        Axios.get('https://rest.bandsintown.com/artists/' + searchStr + '/events?app_id=codingbootcamp')
+            .then(function(response) {
+                console.log(response.data);
+
+            }).catch(function(error) {
+                if (error) {
+                    console.log(error);
+                }
+            })
+
         break;
 
     case 'spotify-this-song':
@@ -38,15 +69,6 @@ switch (input) {
     
 }
 
-/* var args = process.argv;
-// use something like this if you wanted 
-// to run mutliple commands at the same time
-for (var i = 2; i < args.length; i++) {
 
-    switch(input) {
-        case 'concert-this':
-            console.log('You selected concert-this');
 
-    }
 
-} */
